bccffa79f4fedc21bffe0c95f2e5349c5f16f99f
Paul Boniol <paul.boniol@free.fr>
2024-02-14 15:14:40 +0100

    Update utils.py

--- utils.py
+++ utils.py
@@ -25,13 +25,3 @@ def run_benchmark_frame():
 
 def run_about_frame():
     st.markdown("Here show info on the models, data and us")
-
-
-def show_code(demo):
-    """Showing the code of the demo."""
-    show_code = st.sidebar.checkbox("Show code", True)
-    if show_code:
-        # Showing the code of the demo.
-        st.markdown("## Code")
-        sourcelines, _ = inspect.getsourcelines(demo)
-        st.code(textwrap.dedent("".join(sourcelines[1:])))
