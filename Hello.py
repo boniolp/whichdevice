7b77d9da06d64814336fefe0e274f2d60051191c
Paul Boniol <paul.boniol@free.fr>
2024-02-14 15:08:12 +0100

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -14,37 +14,34 @@
 
 import streamlit as st
 from streamlit.logger import get_logger
+from utils import *
 
 LOGGER = get_logger(__name__)
 
 
 def run():
     st.set_page_config(
-        page_title="Hello",
+        page_title="WhichDevice",
         page_icon="👋",
     )
 
-    st.write("# Welcome to Streamlit! 👋")
+    st.write("# Which Device?")
 
-    st.sidebar.success("Select a demo above.")
 
     st.markdown(
         """
-        Streamlit is an open-source app framework built specifically for
-        Machine Learning and Data Science projects.
-        **👈 Select a demo from the sidebar** to see some examples
-        of what Streamlit can do!
-        ### Want to learn more?
-        - Check out [streamlit.io](https://streamlit.io)
-        - Jump into our [documentation](https://docs.streamlit.io)
-        - Ask a question in our [community
-          forums](https://discuss.streamlit.io)
-        ### See more complex demos
-        - Use a neural net to [analyze the Udacity Self-driving Car Image
-          Dataset](https://github.com/streamlit/demo-self-driving)
-        - Explore a [New York City rideshare dataset](https://github.com/streamlit/demo-uber-nyc-pickups)
+        Welcom to WhichDevice! This tools will help you to...
     """
     )
+
+    with Playground:
+        run_playground_frame()
+
+    with tab_benchmark:
+        run_benchmark_frame()
+
+    with tab_about:
+        run_about_frame()
 
 
 if __name__ == "__main__":
