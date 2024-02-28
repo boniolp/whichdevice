a395545adc7e7ad6fed504336adc39d28d16e701
Paul Boniol <paul.boniol@free.fr>
2024-02-28 16:01:17 +0100

    Update utils.py

--- utils.py
+++ utils.py
@@ -79,7 +79,7 @@ def run_playground_frame():
     n_win = len(df) // window_size
 
     with colcontrol_2:
-        st.markdown("<p style='text-align: center;'>from {} to {} <\p>".format(df.iloc[CURRENT_WINDOW*window_size: (CURRENT_WINDOW+1)*window_size].index[0],df.iloc[CURRENT_WINDOW*window_size: (CURRENT_WINDOW+1)*window_size].index[-1]))
+        st.markdown("<p style='text-align: center;'>from {} to {} <\p>".format(df.iloc[CURRENT_WINDOW*window_size: (CURRENT_WINDOW+1)*window_size].index[0],df.iloc[CURRENT_WINDOW*window_size: (CURRENT_WINDOW+1)*window_size].index[-1]),unsafe_allow_html=True)
     
     if CURRENT_WINDOW > n_win:
         CURRENT_WINDOW=n_win
