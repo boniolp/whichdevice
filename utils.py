a58eef165de4674f2500da1b94632486d492d141
Adrien Petralia <adrien.petralia@gmail.com>
2024-03-29 16:41:15 +0100

    Update utils.py

--- utils.py
+++ utils.py
@@ -713,9 +713,9 @@ def plot_cam(k, df, window_size, appliances, pred_dict_all):
     xaxis_title_dict = {f'xaxis{len(appliances)}_title': 'Time'}
     fig_cam.update_layout(title='Detail of explainable pattern localization for each classifier', **xaxis_title_dict)
     fig_cam.update_layout(legend=dict(orientation='h', x=0.5, xanchor='center', y=-0.1),
-                          height=80 + 30 + 180 * len(appliances),
+                          height=150 + 200 * len(appliances),
                           width=1000,
-                          margin=dict(l=100, r=20, t=50, b=30))
+                          margin=dict(l=110, r=20, t=70, b=50))
 
     return fig_cam
 
